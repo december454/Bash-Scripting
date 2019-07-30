36fce1dc7f39592c37345993c2b4605c675c7bc6
Griffin White <griffinowhite@gmail.com>
2019-07-30 15:45:26 -0500

    Adding comments to the interest program.

--- Rewritten Programs/Interest.sh
+++ Rewritten Programs/Interest.sh
@@ -1,14 +1,22 @@
 #! /bin/bash
 
+#Asking the user to enter the loan amount.
 echo "Please enter the amount fo the loan."
+#Taking in their input.
 read loanAmount
 
+#Asking the user to enter their interest rate.
 echo -e "\nPlease enter the interest rate."
+#Taking in their input.
 read interestRate
 
+#Asking the user to enter how many years the loan is for.
 echo -e "\nPlease enter the number of years the loan is for."
+#Taking in their input.
 read loanYears
 
+#Calculating the total amount of interest they will pay on the loan. Using the bc calculator for float operations.
 totalInterest=`echo "$loanAmount * $interestRate * $loanYears / 100" | bc`
 
+#Telling the user how much they will spend on interest for the loan.
 echo -e "\nYou spent $totalInterest dollars on interest over the past $loanYears years."
