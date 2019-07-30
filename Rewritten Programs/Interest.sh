4b81d0ce7e5fd8dbd4b6fd9d1b5cdbda19b23f41
Griffin White <griffinowhite@gmail.com>
2019-07-30 13:15:43 -0500

    Finished rewirtting Interest program as a Bash script.

---
+++ Rewritten Programs/Interest.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+echo "Please enter the amount fo the loan."
+read loanAmount
+
+echo -e "\nPlease enter the interest rate."
+read interestRate
+
+echo -e "\nPlease enter the number of years the loan is for."
+read loanYears
+
+totalInterest=`echo "$loanAmount * $interestRate * $loanYears / 100" | bc`
+
+echo -e "\nYou spent $totalInterest dollars on interest over the past $loanYears years."
+
+
+
+
+
